27c8d41de83c9a73d617476533fd422c81ee16be
YuanZhang1203 <56345266+YuanZhang1203@users.noreply.github.com>
2019-10-15 19:58:17 +0100

    Add files via upload

---
+++ Week2/Code/dictionary.py
@@ -0,0 +1,23 @@
+taxa = [ ('Myotis lucifugus','Chiroptera'),
+         ('Gerbillus henleyi','Rodentia',),
+         ('Peromyscus crinitus', 'Rodentia'),
+         ('Mus domesticus', 'Rodentia'),
+         ('Cleithrionomys rutilus', 'Rodentia'),
+         ('Microgale dobsoni', 'Afrosoricida'),
+         ('Microgale talazaci', 'Afrosoricida'),
+         ('Lyacon pictus', 'Carnivora'),
+         ('Arctocephalus gazella', 'Carnivora'),
+         ('Canis lupus', 'Carnivora'),
+        ]
+
+# Write a short python script to populate a dictionary called taxa_dic 
+# derived from  taxa so that it maps order names to sets of taxa. 
+# E.g. 'Chiroptera' : set(['Myotis lucifugus']) etc.
+
+# Dict operation
+res = dict()
+for i in range(10):
+    if taxa[i][1] not in res:
+        res[taxa[i][1]] = []
+    res[taxa[i][1]].append(taxa[i][0])
+print(res)
